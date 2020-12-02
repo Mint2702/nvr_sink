da31ee5832a204c4ae6adda2c0f07a30d24c2aad
kuderr <dakudryavcev@gmail.com>
2020-12-03 02:34:44 +0300

    fix

--- gcalendar_ruz/core/apis/nvr_api.py
+++ gcalendar_ruz/core/apis/nvr_api.py
@@ -6,4 +6,9 @@ API_URL = "https://nvr.miem.hse.ru/api/erudite"
 def get_course_emails(course_code):
     res = requests.get(f"{API_URL}/disciplines", params={"course_code": course_code})
     data = res.json()
-    return data.get("emails")
+    grp_emails = data.get("emails")
+
+    if grp_emails == ['']
+        return None
+
+    return grp_emails
