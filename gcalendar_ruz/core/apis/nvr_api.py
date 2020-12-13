f81a3afd02eee53ec351c34b5b5b515b7109355a
kuderr <dakudryavcev@gmail.com>
2020-12-13 21:16:33 +0300

    fix nvr erudite response parse

--- gcalendar_ruz/core/apis/nvr_api.py
+++ gcalendar_ruz/core/apis/nvr_api.py
@@ -5,7 +5,8 @@ API_URL = "https://nvr.miem.hse.ru/api/erudite"
 
 def get_course_emails(course_code):
     res = requests.get(f"{API_URL}/disciplines", params={"course_code": course_code})
-    data = res.json()
+    
+    data = res.json()['data']
     grp_emails = data.get("emails")
 
     if grp_emails == ['']:
