83d3ccd95bcacb70823da3297c32cad4f27baa6e
danil <danil.mirtalibov.27@mail.ru>
2021-01-15 17:28:40 +0300

    refresh_token updated

--- gcalendar_ruz/core/apis/calendar_api.py
+++ gcalendar_ruz/core/apis/calendar_api.py
@@ -13,22 +13,18 @@ from ..utils import semlock, GOOGLE, token_check
 
 CREDS_PATH = "core/creds/credentials.json"
 TOKEN_PATH = "core/creds/tokenCalendar.pickle"
+SCOPES = "https://www.googleapis.com/auth/calendar"
 
 
 class GCalendar:
     SERVICE = GOOGLE
 
-    def __init__(
-        self,
-        creds_path: str = CREDS_PATH,
-        token_path: str = TOKEN_PATH,
-        scopes: str or list = "https://www.googleapis.com/auth/calendar",
-    ):
+    def __init__(self):
         """
         Setting up calendar
         """
 
-        self.refresh_token(creds_path, token_path, scopes)
+        self.refresh_token()
 
         self.HEADERS = {
             "Content-Type": "application/json",
@@ -37,23 +33,18 @@ class GCalendar:
 
         self.service = build("calendar", "v3", credentials=self.creds)
 
-    def refresh_token(
-        self,
-        creds_path: str,
-        token_path: str,
-        scopes: str or list = "https://www.googleapis.com/auth/calendar",
-    ):
+    def refresh_token(self):
         self.creds = None
-        if os.path.exists(token_path):
-            with open(token_path, "rb") as token:
+        if os.path.exists(TOKEN_PATH):
+            with open(TOKEN_PATH, "rb") as token:
                 self.creds = pickle.load(token)
         if not self.creds or not self.creds.valid:
             if self.creds and self.creds.expired and self.creds.refresh_token:
                 self.creds.refresh(Request())
             else:
-                flow = InstalledAppFlow.from_client_secrets_file(creds_path, scopes)
+                flow = InstalledAppFlow.from_client_secrets_file(CREDS_PATH, SCOPES)
                 self.creds = flow.run_local_server(port=0)
-            with open(token_path, "wb") as token:
+            with open(TOKEN_PATH, "wb") as token:
                 pickle.dump(self.creds, token)
 
     @token_check
